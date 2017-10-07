737a635ed7cf21645842e8cb6d7a52ddd2e929a1
Marko Krüger <>
2017-10-08 00:23:27 +0200

    fixing assets path (again, and again.)

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -17,22 +17,16 @@ class AppController
         $indexHtml = file_get_contents(public_path('frontend/index.html'));
 
         $pattern = [
-            '~/assets/vendor(.*).css~',
-            '~/assets/frontend(.*).css~',
-            '~/assets/vendor(.*).js~',
-            '~/assets/frontend(.*).js~',
+            '~/(assets/.*\.(css|js))~',
             '~/ember-cli-live-reload.js~'
         ];
 
         $replacements = [
-            'frontend/assets/vendor.css',
-            'frontend/assets/frontend.css',
-            'frontend/assets/vendor.js',
-            'frontend/assets/frontend.js',
+            '/frontend/$1',
             "http://127.0.0.1:4200/ember-cli-live-reload.js"
         ];
 
-        $indexHtml = preg_replace_array($pattern, $replacements, $indexHtml);
+        $indexHtml = preg_replace($pattern, $replacements, $indexHtml);
 
 
         return view('app', ['app' => $indexHtml]);
