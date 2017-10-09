882878d135b0759c231570589cf36834316e281c
Krueger, Marko <marko.krueger@mdc-berlin.de>
2017-10-09 10:27:59 +0200

    removes bower ember-fontawesome

--- resources/assets/frontend/ember-cli-build.js
+++ resources/assets/frontend/ember-cli-build.js
@@ -3,12 +3,15 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     'ember-bootstrap': {
       bootstrapVersion: 4,
       importBootstrapFont: false,
       importBootstrapCSS: false
+    },
+    'ember-font-awesome': {
+      useScss: true
     }
   });
 
