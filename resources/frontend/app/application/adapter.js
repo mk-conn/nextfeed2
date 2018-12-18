365f9e14408ea51806c625d08212d9e9bbb50be7
Marko Krüger <mk@marko-krueger.de>
2018-12-18 16:07:23 +0100

    making it laravel 5.7 and ember 3.5 ready and passport and zend-feed ready * fix

--- resources/frontend/app/application/adapter.js
+++ resources/frontend/app/application/adapter.js
@@ -1,8 +1,11 @@
+import { inject as service } from '@ember/service';
+import { isPresent } from '@ember/utils';
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   namespace: 'api/v1',
+  session: service(),
   /**
    *
    * @param xhr
