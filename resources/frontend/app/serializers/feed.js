ed58877ffb29301accc2b43a4b6d520a739d15e4
Marko Krüger <>
2019-01-04 00:39:41 +0100

    fixed feed settings update not always working

--- resources/frontend/app/serializers/feed.js
+++ resources/frontend/app/serializers/feed.js
@@ -1,4 +1,5 @@
 import ApplicationSerializer from 'frontend/application/serializer';
+import { isPresent } from '@ember/utils';
 
 export default ApplicationSerializer.extend({
   normalize(modelClass, resourceHash) {
@@ -9,5 +10,19 @@ export default ApplicationSerializer.extend({
     }
 
     return this._super(...arguments);
+  },
+  /**
+   *
+   * @param snapshot
+   * @param options
+   * @returns {*|never}
+   */
+  serialize(snapshot, options) {
+    let json = this._super(snapshot, options);
+
+    if (isPresent(json.data.attributes.meta)) {
+      delete json.data.attributes.meta;
+    }
+    return json;
   }
 });
